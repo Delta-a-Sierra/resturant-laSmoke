d50c282bc31ee5266ce286e7849b6a762729e16a
Dwayne Sutherland <dsutherland13@hotmail.co.uk>
2020-12-08 22:43:09 +0000

    Webpack plugins and loaders setup

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./styles/main.scss"
+import img from './assets/imgs/Logo.png'
+
+console.log('change')
---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry : './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: './src/template.html'
+    })],
+    module:{
+        rules: [
+            {
+                test: /\.html$/i,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        publicPath: 'assets/imgs',
+                        outputPath: "assets/imgs",
+                        esModule: false,
+                    }
+                }
+            }
+            
+        ] 
+    }
+};
